b6e91f7257c92b6aa5ad777928153d154f577d24
LigualLink <lixiangban1023@qq.com>
2023-12-20 11:43:55 +0800

    把登陆逻辑单独写到文件里面

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,18 @@
+import 'package:linguapp/services/auth/auth_user.dart';
+
+abstract class AuthenProvider {
+  AuthUser? get currentUser;
+
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:linguapp/services/auth/auth_provider.dart';
+import 'package:linguapp/services/auth/auth_user.dart';
+
+class AuthService implements AuthenProvider {
+  final AuthenProvider provider;
+
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+}
